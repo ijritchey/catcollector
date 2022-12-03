27e862510c147ab58bbb9a099722839ab12d615a
ijritchey <ijritchey@hotmail.com>
2022-12-03 10:39:05 -0800

    fixed database

--- main_app/views.py
+++ main_app/views.py
@@ -1,37 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# models being imported
+from .models import Cat
 
 # temp add Cats class
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-    def __str__(self):
-        return f"{self.name}"
-
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
-
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', { 'cats': cats })
+    cats = list(Cat.objects.all())
+    return render(request, 'index.html', {'cats': cats})
+
 
 def about(request):
     return render(request, 'about.html')
 
-def contact(request): 
-    return render(request,'contact.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
 
 def blog(request):
     return render(request, 'blog.html')
 
+
 def cats_index(request):
+    cats = list(Cat.objects.all())
     return render(request, 'cats/index.html', { 'cats': cats })
